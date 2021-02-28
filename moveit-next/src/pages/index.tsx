0ea31ed2fbbd8df75ec0d9566668d08940df2e45
Anderson Barros Silva <anderson.tec12@gmail.com>
2021-02-27 22:34:11 -0300

    end day 2

---
+++ moveit-next/src/pages/index.tsx
@@ -0,0 +1,28 @@
+import { CompleteChallenges } from "../components/CompleteChallenges";
+import { ExperienceBar } from "../components/ExperienceBar";
+import {Profile} from '../components/Profile'
+import {Countdown} from '../components/Countdown'
+
+import styles from  '../styles/pages/Home.module.css'
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>inicio | move.it</title>
+      </Head>
+      <ExperienceBar/>
+
+      <section>
+        <div >
+          <Profile />
+          <CompleteChallenges />
+          <Countdown />
+        </div>
+
+        <div></div>
+      </section>
+    </div>
+  )
+}
